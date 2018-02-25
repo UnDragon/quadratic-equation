89e6c146365a7c5b1195371822c57ae88f1a258f
UnDragon <31067730+UnDragon@users.noreply.github.com>
2018-02-25 17:25:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.replace(/\s+/g, "");
+    var coefficient = equation.match(/(-?\d+)/gi);
+    coefficient.splice(1, 1);
+    var discriminant = Math.round((Math.pow(coefficient[1], 2)) - 4 * coefficient[0] * coefficient[2]);
+    if (discriminant>0){
+        return [Math.round(-coefficient[1] / ( 2 * coefficient[0] ) - Math.sqrt( discriminant ) / ( 2 * coefficient[0] )),
+            Math.round(-coefficient[1] / ( 2 * coefficient[0] ) + Math.sqrt( discriminant ) / ( 2 * coefficient[0] ))]
+            .sort(function (a, b) {
+                if (a > b){
+                    return 1;
+                }
+                else if (a == b){
+                    return 0;
+                }
+                else {
+                    return -1;
+                }
+            });
+    }
+    else if (discriminant == 0){
+        return[Math.round(-coefficient[1] / (2*coefficient[0])), Math.round(-coefficient[1]  / (2*coefficient[0]))]
+            .sort(function (a, b) {
+                if (a > b){
+                    return 1;
+                }
+                else if (a == b){
+                    return 0;
+                }
+                else {
+                    return -1;
+                }
+            });
+    }
+    else {
+        return [Math.round(-coefficient[1] / (2 * coefficient[0])), Math.round(-Math.sqrt(-discriminant) / (2 * coefficient[0]))]
+            .sort(function (a, b) {
+            if (a > b){
+                return 1;
+            }
+            else if (a == b){
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        });
+    }
 }
